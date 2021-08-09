b4ff6620757b5534286d27a458a437c935b05a05
MrCowFace <59069516+MrCowFace@users.noreply.github.com>
2021-08-09 02:31:58 -0400

    fixed movement

--- Assets/Scripts/GetMinDotNode.cs
+++ Assets/Scripts/GetMinDotNode.cs
@@ -12,7 +12,26 @@ public class GetMinDotNode : MonoBehaviour
     {
     	return (stairsMask & (1 << layer)) == 0;
     }
-    	
-    	
+
+
+    public int ConditionalLeaps(bool a, bool b, bool c)
+    {
+	    if (a)
+	    {
+		    return 1;
+	    }
+	    else if (b)
+	    {
+		    return 2;
+	    }
+	    else if (c)
+	    {
+		    return 3;
+	    }
+	    else
+	    {
+		    return 4; 
+	    }
+    }
     	
 }
